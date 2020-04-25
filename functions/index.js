53ebf6d1cbde138ac1316b0f992e9a2b9250aa28
Claudia Vizena <cvizena@Claudias-MacBook-Pro.local>
2020-04-24 17:29:33 -0700

    Create authentication

--- functions/index.js
+++ functions/index.js
@@ -21,35 +21,75 @@ firebase.initializeApp(config)
 const db = admin.firestore();
 
 app.get('/screams', (req, res) => {
-    db
-    .collection('screams')
-    .orderBy('createdAt', 'desc')
-    .get()
-    .then((data) => {
-      let screams = [];
-      data.forEach((doc) => {
-        screams.push({
-          screamID: doc.id,
-          body: doc.data().body,
-          userHandle: doc.data().userHandle,
-          createdAt: doc.data().createAt,
-          commentCount: doc.data().commentCount,
-          likeCount: doc.data().likeCount
-        });
+  db
+  .collection('screams')
+  .orderBy('createdAt', 'desc')
+  .get()
+  .then((data) => {
+    let screams = [];
+    data.forEach((doc) => {
+      screams.push({
+        screamID: doc.id,
+        body: doc.data().body,
+        userHandle: doc.data().userHandle,
+        createdAt: doc.data().createAt,
+        commentCount: doc.data().commentCount,
+        likeCount: doc.data().likeCount
       });
-      return res.json(screams);
-    })
-    .catch((err) => console.error(err));
-})
+    });
+    return res.json(screams);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).json({ error: err.code });
+  });
+});
 
-app.post('/scream', (req, res) => {
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+      idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+      console.error('No token found')
+      return res.status(403).json({ error: 'Unauthorized' });
+    }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch(err => {
+      console.error('Error while verifying token', err);
+      return res.status(403).json(err);
+    });
+};
+
+
+// Post one scream
+app.post('/scream', FBAuth, (req, res) => {
+  if (req.body.body.trim() === '') {
+    return res.status(400).json({ body: 'Body must not be empty'});
+  }
+
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString()
   };
 
-    db
+  db
     .collection('screams')
     .add(newScream)
     .then((doc) => {
@@ -124,7 +164,7 @@ app.post('/signup', (req, res) => {
       };
       return db.doc(`/users/${newUser.handle}`).set(userCredentials);
     })
-    .then((data) => {
+    .then(() => {
       return res.status(201).json({ token });
     })
     .catch(err => {
