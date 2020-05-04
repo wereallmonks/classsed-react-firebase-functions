4b6c270ed15fae21d551c194c3f1024fe14aaff6
Claudia Vizena <cvizena@Claudias-MacBook-Pro.local>
2020-05-04 12:53:30 -0700

    Refactor and organize.

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyDUwXK_tOHVeidcJZ9jahJoFhE8uF6CLEg",
+    authDomain: "socialape-54d3c.firebaseapp.com",
+    databaseURL: "https://socialape-54d3c.firebaseio.com",
+    projectId: "socialape-54d3c",
+    storageBucket: "socialape-54d3c.appspot.com",
+    messagingSenderId: "133406317230",
+    appId: "1:133406317230:web:88c751bf867fb6047feb3f",
+    measurementId: "G-TTWQZT416T"
+  };
